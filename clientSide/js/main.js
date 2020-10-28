87da92f856704530a394a36fd2f6405644f37e19
ali masoomzadeh <eng.masoomzadeh@gmail.com>
2020-10-28 19:03:32 +0330

    function checkUserName function postDataDB function validations

--- clientSide/js/main.js
+++ clientSide/js/main.js
@@ -1,7 +1,121 @@
 $(document).ready(function () {
-
-
     $("#signIn").click(function () {
         location.href = "clientSide/pages/user/signIn.html"
     });
 });
+
+function checkUserName(url, userName) {
+    if (userName !== undefined && userName !== "") {
+        let returnValue;
+        $.ajax({
+            url: url + userName,
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                if (data.length !== 0) {
+                    returnValue = true;
+                } else {
+                    returnValue = false;
+                }
+            },
+            error: function (xhr, status, error) {
+                let err = eval("(" + xhr.responseText + ")");
+                returnValue = err.Message;
+            },
+            async: false
+        });
+        return returnValue;
+    }
+}
+
+function getDataDB(url) {
+    let returnValue;
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            if (data) {
+                returnValue = data;
+            }
+        },
+        error: function (xhr, status, error) {
+            let err = eval("(" + xhr.responseText + ")");
+            returnValue = err.Message;
+        },
+        async: false
+    });
+    return returnValue;
+}
+
+function postDataDB(url, data) {
+    let returnValue;
+    $.ajax({
+        url: url,
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        success: function (data) {
+            returnValue = data;
+        },
+        error: function (xhr, status, error) {
+            let err = eval("(" + xhr.responseText + ")");
+            returnValue = err.Message;
+        },
+        async: false
+    });
+    return returnValue;
+}
+
+function validations(name, type, value) {
+    $("#" + name + "Error").html("");
+    if (value !== undefined && value !== "") {
+        switch (type) {
+            case "email":
+                if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value)) {
+                    let dataDB = checkUserName("http://localhost:3000/users?userName=", $("#inputUserName").val());
+                    console.log(dataDB)
+                    if (dataDB === true) {
+                        $("#userNameError").html("کلمه کاربری مورد نظر قبلا ثبت شده است");
+                        return false;
+                    }else{
+                        $("#userNameError").html("");
+                        return true;
+                    }
+                } else {
+                    $("#" + name + "Error").html("فرمت مورد نظر اشتباه می باشد");
+                    return false;
+                }
+                break
+            case "text":
+                if (/^[A-Za-z]+$/.test(value)) {
+                    $("#" + name + "Error").html("");
+                    return true;
+                } else {
+                    $("#" + name + "Error").html("فرمت فیلد مورد نظر اشتباه می باشد");
+                    return false;
+                }
+                break
+            case "password":
+                if (value.match(/[0-9]/g))  {
+                    if (value.length >= 8)  {
+                        $("#" + name + "Error").html("");
+                        return true;
+                    } else {
+                        $("#" + name + "Error").html("طول کلمه عبور باید مساوی یا بزرگتر از 8 کاراکتر باشد ");
+                        return false;
+                    }
+                } else {
+                    $("#" + name + "Error").html("کلمه عبور فقط باید عدد باشد ");
+                    return false;
+                }
+                break
+        }
+    } else {
+        $("#" + name + "Error").html("این فیلد اجباریست");
+        return false;
+    }
+}
